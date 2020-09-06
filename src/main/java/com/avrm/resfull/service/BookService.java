21ab0a517cd7106aaaffc6884226a713d180fb5c
avrm <avrmeduard@gmail.com>
2020-09-06 21:38:26 +0300

    BookService class added

---
+++ src/main/java/com/avrm/resfull/service/BookService.java
@@ -0,0 +1,19 @@
+package com.avrm.resfull.service;
+
+import com.avrm.resfull.model.Book;
+import com.avrm.resfull.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository repository;
+
+
+
+
+
+
+}
