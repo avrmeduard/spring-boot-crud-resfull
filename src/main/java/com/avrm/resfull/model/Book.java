1ad55264949736aeaa74b483c5b71ad8d6f4835e
avrm <avrmeduard@gmail.com>
2020-09-06 21:04:04 +0300

    Book model added

--- src/main/java/com/avrm/resfull/model/Book.java
+++ src/main/java/com/avrm/resfull/model/Book.java
@@ -1,4 +1,101 @@
 package com.avrm.resfull.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "book_tbl")
 public class Book {
+
+    @Id
+    @GeneratedValue
+    private Integer bookId;
+
+    private String title;
+    private String author;
+    private String publisher;
+    private String isbn;
+    private Integer numberOfPages;
+    private String language;
+    private String genre;
+
+    public Book() {
+    }
+
+    public Book(Integer bookId, String title, String author, String publisher, String isbn, Integer numberOfPages, String language, String genre) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.isbn = isbn;
+        this.numberOfPages = numberOfPages;
+        this.language = language;
+        this.genre = genre;
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(Integer numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
