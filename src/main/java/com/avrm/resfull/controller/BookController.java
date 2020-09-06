497936208427dbb37afc605b8e069cd9286240d9
avrm <avrmeduard@gmail.com>
2020-09-06 23:36:51 +0300

    restController book class added

---
+++ src/main/java/com/avrm/resfull/controller/BookController.java
@@ -0,0 +1,20 @@
+package com.avrm.resfull.controller;
+
+import com.avrm.resfull.model.Book;
+import com.avrm.resfull.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    @Autowired
+    private BookService service;
+
+
+
+}
