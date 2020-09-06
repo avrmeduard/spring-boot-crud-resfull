a10175a1011e7ba553591484c02a29e0658f7f32
avrm <avrmeduard@gmail.com>
2020-09-06 21:44:04 +0300

    save a list of books method added

--- src/main/java/com/avrm/resfull/service/BookService.java
+++ src/main/java/com/avrm/resfull/service/BookService.java
@@ -5,6 +5,8 @@ import com.avrm.resfull.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookService {
 
@@ -15,6 +17,15 @@ public class BookService {
     public Book saveBook(Book book) {
         return repository.save(book);
     }
+
+    public List<Book> saveBooks(List<Book> books) {
+        return repository.saveAll(books);
+    }
+
+
+
+
+
 
 
 }
