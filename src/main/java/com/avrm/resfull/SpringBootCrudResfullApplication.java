80e4f49cda43cc861b8dc0f7e0fab0f5cc1b984a
avrm <avrmeduard@gmail.com>
2020-09-06 21:01:23 +0300

    Initial commit

---
+++ src/main/java/com/avrm/resfull/SpringBootCrudResfullApplication.java
@@ -0,0 +1,13 @@
+package com.avrm.resfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudResfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudResfullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avrm/resfull/SpringBootCrudResfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avrm.resfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudResfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
